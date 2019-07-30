97bfd9caff9a4ca066197940294ab7b6b828e629
trungkienbkhn <trungkienbkhn123@gmail.com>
2019-07-30 13:51:52 +0700

    comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,70 @@
+class CommentsController < ApplicationController
+  before_action :logged_in_user
+  before_action :load_commentable, only: :create
+  before_action :correct_comment, only: :destroy
+
+  def create
+    @id = params[:comment][:parent_id].to_i
+
+    if @id > 0
+      parent = @commentable.comments.find_by(id: @id)
+      @comment = parent.children.build comment_params
+      @comment.user_id = current_user.id
+      @comment.commentable_id = @commentable.id
+      @comment.commentable_type = parent.commentable_type
+    else
+      @comment = @commentable.comments.build comment_params
+      @comment.user_id = current_user.id
+    end
+
+    if @comment.save
+      flash[:success] = t "create_success"
+
+      if @comment.commentable_type == Settings.comment_type
+        redirect_to blog_path(id: @comment.commentable_id)
+      end
+    else
+      flash[:danger] = t "create_fail"
+      redirect_to blog_path(id: @comment.commentable_id)
+    end
+  end
+
+  def destroy
+    @comment.descendants.each(&:destroy)
+
+    if @comment.destroy
+      flash[:success] = t "delete_success"
+
+      if @comment.commentable_type == Settings.comment_type
+        redirect_to blog_path(id: @comment.commentable_id)
+      end
+    else
+      flash[:danger] = t "delete_fail"
+      redirect_to blog_path(id: @comment.commentable_id)
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit :content
+  end
+
+  def load_commentable
+    if params[:comment][:type] == Settings.comment_type
+      @commentable = Blog.find_by id: params[:comment][:commentable_id]
+    end
+
+    return if @commentable
+    flash[:danger] = t "not_found"
+    redirect_to root_url
+  end
+
+  def correct_comment
+    @comment = current_user.comments.find_by id: params[:id]
+
+    return if @comment
+    flash[:danger] = t "not found"
+    redirect_to root_url
+  end
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,9 @@
 class Comment < ApplicationRecord
+  acts_as_tree order: "created_at ASC"
   has_one :general
+  belongs_to :user
+  belongs_to :commentable, polymorphic: true
+
+  validates :content, presence: true,
+    length: {maximum: Settings.maximum_length_comment}
 end
