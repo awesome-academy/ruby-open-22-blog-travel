947c598bca50b2c8c46825f3db227da81213b467
HungDam <bestdothua@gmail.com>
2019-08-15 14:11:02 +0700

    back up project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,54 @@
 User.create(
   name: "Admin",
-  email: "admin123456@gmail.com",
+  email: "admin@blogtravel.com",
   password: "123456",
   password_confirmation: "123456",
   is_admin: true
 )
 
-5.times do |i|
+30.times do |i|
   User.create([{
     name: Faker::Name.name,
     email: Faker::Internet.email,
-    #remote_avatar_url: "https://randomuser.me/api/portraits/#{["men", "women"].sample}/#{i}.jpg",
-    #password: Faker::Internet.password
+    remote_avatar_url: "https://randomuser.me/api/portraits/#{["men", "women"].sample}/#{i}.jpg",
+    password: Faker::Internet.password
   }])
 end
 
-name_district = ["Ho Hoan Kiem", "Hai Ba Trung", "Hoang Mai", "Tay Bac"]
+name_district = ["Hoan Kiem", "Hai Ba Trung", "Hoang Mai", "Tay Ho", "Long Bien", "Ha Dong", "Bac Tu Liem", "Nam Tu Liem"]
 
 name_district.each do |name|
   District.create!(name: name)
 end
+
+10.times do
+  Blog.create([{
+      user_id: Faker::Number.between(from: User.first.id, to: User.last.id),
+      district_id: Faker::Number.between(from: District.first.id, to: District.last.id),
+      remote_cover_photo_url: "https://loremflickr.com/800/500/travel",
+      topic: Faker::Nation.nationality,
+      address: Faker::Address.street_address,
+      content: Faker::Markdown.emphasis
+      }])
+end
+
+10.times do
+  Place.create([{
+    name: Faker::Nation.capital_city,
+    description: Faker::Dessert.flavor,
+    address: Faker::Address.street_address,
+    vote: Faker::Number.between(from: 1, to: 5),
+    cost: (Faker::Number.between(from: 1, to: 5)*100).to_s,
+    district_id: Faker::Number.between(from: District.first.id, to: District.last.id),
+    remote_photo_url: "https://loremflickr.com/800/500/capital",
+    status: ["hotel", "restaurant"].sample,
+  }])
+end
+
+10.times do
+  Relationship.create(
+    follower_id: Faker::Number.between(from: User.first.id, to: User.last.id),
+    followed_id: Faker::Number.between(from: User.first.id, to: User.last.id),
+  )
+end
+
